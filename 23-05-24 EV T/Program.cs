0553edb2abb6074d809f27dbdfb05135ad45774b
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-23 10:45:56 +0400

    Eyni reqemden yan-yana duplicate varsa, bunu bize bildirsin

--- 23-05-24 EV T/Program.cs
+++ 23-05-24 EV T/Program.cs
@@ -27,35 +27,52 @@ namespace _23_05_24_EV_T
             #region Common element between the two arrays
             /* TASK 2 -  Birinci array daxilinde olan butun reqemler, 2 ci array daxilinde var ya yox?*/
 
-            int[] numbers = { 1, 2, 3, 4, };
-            int[] numbers2 = { 1, 3, 5, 6, };
-            bool commonElement = false;
+            //int[] numbers = { 1, 2, 3, 4, };
+            //int[] numbers2 = { 1, 3, 5, 6, };
+            //bool commonElement = false;
 
-            for (int i = 0; i < numbers.Length; i++)
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    for (int j = 0; j < numbers2.Length; j++)
+            //    {
+
+            //          if (numbers[i] == numbers2[j])
+            //          {
+            //            commonElement = true;
+            //            break;
+            //          }
+            //         else
+            //         {
+            //            commonElement = false;
+
+            //         }
+            //    }
+            //}
+
+            //if (commonElement = true)
+            //{
+            //    Console.WriteLine("There is a common element between the two arrays.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There is no common element between the two arrays");
+            //}
+            #endregion
+
+
+            #region back-to-back duplicate number
+            /* TASK 3 - Eyni reqemden yan-yana duplicate varsa, bunu bize bildirsin */
+            int[] numbers = { 1, 2, 4, 4, 5, };
+            bool duplicateNumber = false;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
-                for (int j = 0; j < numbers2.Length; j++)
+                if (numbers[i] == numbers[i + 1])
                 {
-
-                    if (numbers[i] == numbers2[j])
-                    {
-                        commonElement = true;
-                        break;
-                    }
-                    else
-                    {
-                        commonElement = false;
-
-                    }
+                    duplicateNumber = true;
+                    Console.WriteLine($"Duplicate number found: {numbers[i]}");
                 }
-            }
 
-            if (commonElement = true)
-            {
-                Console.WriteLine("There is a common element between the two arrays.");
-            }
-            else
-            {
-                Console.WriteLine("There is no common element between the two arrays");
             }
             #endregion
 
