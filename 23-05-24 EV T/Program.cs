cfe59e1d6a1d4f6a98da8af1ad5c902898e5bfcd
Fatima <fatimavaliyeva0704@gmail.com>
2024-05-23 10:45:29 +0400

    birinci array daxilinde olan butun reqemler, 2ci array daxilinde var ya yox?

--- 23-05-24 EV T/Program.cs
+++ 23-05-24 EV T/Program.cs
@@ -8,19 +8,54 @@ namespace _23_05_24_EV_T
         {
             #region Reverse Array
             /* TASK 1 - Arrayin tersine cevrilmesi*/
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            if (true)
+            //if (true)
+            //{
+            //    int[] modifiedNumbers = new int[numbers.Length];
+            //    for (int i = numbers.Length - (1); i >= 0; i--)
+            //    {
+            //        modifiedNumbers[numbers.Length - 1 - i] = numbers[i];
+            //    }
+            //    for (int i = 0; i < modifiedNumbers.Length; i++)
+            //    {
+            //        Console.Write(modifiedNumbers[i] + " ");
+            //    }
+            //}
+            #endregion
+
+            #region Common element between the two arrays
+            /* TASK 2 -  Birinci array daxilinde olan butun reqemler, 2 ci array daxilinde var ya yox?*/
+
+            int[] numbers = { 1, 2, 3, 4, };
+            int[] numbers2 = { 1, 3, 5, 6, };
+            bool commonElement = false;
+
+            for (int i = 0; i < numbers.Length; i++)
             {
-                int[] modifiedNumbers = new int[numbers.Length];
-                for (int i = numbers.Length - (1); i >= 0; i--)
+                for (int j = 0; j < numbers2.Length; j++)
                 {
-                    modifiedNumbers[numbers.Length - 1 - i] = numbers[i];
+
+                    if (numbers[i] == numbers2[j])
+                    {
+                        commonElement = true;
+                        break;
+                    }
+                    else
+                    {
+                        commonElement = false;
+
+                    }
                 }
-                for (int i = 0; i < modifiedNumbers.Length; i++)
-                {
-                    Console.Write(modifiedNumbers[i] + " ");
-                }
+            }
+
+            if (commonElement = true)
+            {
+                Console.WriteLine("There is a common element between the two arrays.");
+            }
+            else
+            {
+                Console.WriteLine("There is no common element between the two arrays");
             }
             #endregion
 
@@ -31,6 +66,7 @@ namespace _23_05_24_EV_T
 
 
 
+
         }
-}
+    }
 }
